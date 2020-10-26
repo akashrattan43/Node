f7230603708b16aa6d9ff02118edf7d87d17522c
akash rattan <akashrattan43@Gmail.com>
2020-10-25 22:30:50 -0700

    logger

--- app.js
+++ app.js
@@ -6,12 +6,24 @@
 // console.log('TOTAL MEMORY: ' + totalMemory)
 // console.log('FREE MEMORY: ' + freeMemory)
 
-const fs = require('fs')
+// const fs = require('fs')
 
-// const files = fs.readdirSync('./');
-// console.log(files)
+// // const files = fs.readdirSync('./');
+// // console.log(files)
 
-fs.readdir('./', function(err, files){
-    if (err) console.log('Error', err );
-    else console.log('Result', files)
+// fs.readdir('./', function(err, files){
+//     if (err) console.log('Error', err );
+//     else console.log('Result', files)
+// })
+
+const EventEmitter = require('events');
+
+const Logger = require('./logger')
+const logger = new Logger()
+
+//Register a listener
+logger.on('MessageLogged' , (arg) => {
+    console.log('Listener called', arg)
 })
+
+logger.log('message')
